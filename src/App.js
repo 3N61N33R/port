2c7be800d5b381fa35222738b6371a8f754b9dc6
Grace Zawadi <g.zawadi.w@gmail.com>
2022-02-07 18:08:59 +0300

    ft(port):pages
    - about
    - contact
    - navbar
    - skills

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
 
 export default function App() {
-  return "App";
+  return (
+    <main>
+      <About />
+      <Contact />
+      <Navbar />
+      <Skills />
+    </main>
+  );
 }
