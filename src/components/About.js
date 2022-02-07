8c54febfeadf67808b7c51f081f27405683a9f66
Grace Zawadi <g.zawadi.w@gmail.com>
2022-02-07 18:35:34 +0300

    about

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,45 @@
+import React from "react";
+
 export default function About() {
-  return "About";
+  return (
+    <section id="about">
+      <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+        <div className="flex flex-col lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4 font-medium">
+            Hi, I'm 3N61N33R
+            <br className="hidden lg:inline-block" /> I love building amazing
+            apps!
+          </h1>
+          <p className="mb-8 leading-relaxed">
+            Energetic self starter with experience in the IT and service
+            industry. Works well with multicultural and multinational teams.
+            Stays professionally up to date through engagement in developer
+            communities eg Andela Learning Community (ALC), Nairobi Gophers
+            Community and Toastmasters.
+          </p>
+          <div className="flex justify-center">
+            <a
+              href="#contact"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg"
+            >
+              Work With Me
+            </a>
+            <a
+              href="#projects"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
+            >
+              See My Past Work
+            </a>
+          </div>
+        </div>
+        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+          <img
+            className="object-cover object-center rounded"
+            alt="hero"
+            src="./coding.svg"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
